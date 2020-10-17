815caf7350c12dea6fd9051faf4894ae45f7a17d
Otavio Sartorelli de Toledo Piza <70035303+OtavioPiza@users.noreply.github.com>
2020-10-17 16:55:38 -0300

    Button done?

--- untitled-launchpad-emulator/src/Components/Button.jsx
+++ untitled-launchpad-emulator/src/Components/Button.jsx
@@ -49,14 +49,16 @@ class Button extends Component {
 
     render = () => (
         <div className='button'>
-            <Button onMouseDown={this.on} onTouchStart={this.on} className={this.props.class} style={{
+            <Button onMouseDown={this.on} onTouchStart={this.on} className={this.props.class} style={
+                {
                 backgroundCOlor: this.colorOverlay(this.props.color)
-            }}/>
-            <svg width='80' height='80'>
-
-
-            </svg>
-
+                }
+            }>
+                <svg width='80' height='80'>
+                    <path d='M75,80H5a5,5,0,0,1-5-5V5A5,5,0,0,1,5,0H62a8.33,8.33,0,0,1,5.12,2.12L77.88,12.88A8.33,8.33,0,0,1,80,18V75A5,5,0,0,1,75,80Z' fill={this.props.color} stroke='black' stoke-width='2px'/>
+                </svg>
+                {this.props.x.toString() + ' ' + this.props.y.toString()}
+            </Button>
         </div>
     )
 }
