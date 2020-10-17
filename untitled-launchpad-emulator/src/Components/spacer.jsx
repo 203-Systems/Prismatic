ae02ee91519f927f07ffb20121a45e6478548c4e
Otavio Sartorelli de Toledo Piza <70035303+OtavioPiza@users.noreply.github.com>
2020-10-17 18:54:24 -0300

    spacer

---
+++ untitled-launchpad-emulator/src/Components/spacer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import palette from '../palette'
+
+class Spacer extends Component {
+    state = {}
+
+    setColor = (velocity) => {
+
+    }
+
+    render = () => {
+        <div>
+            <Spacer className ='Spacer'/>
+        </div>
+    }
+}
