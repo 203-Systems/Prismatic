3fd2c57cb1c6999a956a8c18cf8e451282dcbfa5
Otavio Sartorelli de Toledo Piza <70035303+OtavioPiza@users.noreply.github.com>
2020-10-17 18:39:19 -0300

    Navbar done? I guess not really sure

--- untitled-launchpad-emulator/src/Components/navbar.jsx
+++ untitled-launchpad-emulator/src/Components/navbar.jsx
@@ -3,9 +3,15 @@ import React, { Component } from 'react'
 class Navbar extends Component {
 
     render = () => (
-        <nav className='navBar'>
-            <a class='nav'/>
-
+        <nav className='navbar-container'>
+            <form className='navbar-form' action={/*TODO*/}>
+                <div className='navbar-input'>
+                    <input type='file' id='file' onChange={this.props.onFileChange}/>
+                </div>
+                <div className='navbar-project'>
+                    <span>Curent project: {this.props.name}</span>
+                </div>
+            </form>
         </nav>
     )
 }
