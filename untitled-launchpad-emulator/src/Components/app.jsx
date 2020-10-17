bf7c8b20d5976daba4dc0bb21450d7fcc421e0b9
NuLL <cainengzhuo@gmail.com>
2020-10-17 18:03:36 -0400

    Restrctured

--- untitled-launchpad-emulator/src/Components/app.jsx
+++ untitled-launchpad-emulator/src/Components/app.jsx
@@ -1,5 +1,22 @@
 import React, { Component } from 'react';
+import ProjectFileReader from './projectFileReader'
 
 class App extends Component {
+  state = {  
+    projectFile: undefined,
+  };
+
+  updateProjectFile = newProjectFile => {
+    this.setState({projectFile: newProjectFile});
+  }
   
+  render() { 
+    return (
+      <React.Fragment>
+        <ProjectFileReader updateProjectFile={this.updateProjectFile}/>
+      </React.Fragment>
+    );
+  }
 }
+
+export default App;
--- untitled-launchpad-emulator/src/index.js
+++ untitled-launchpad-emulator/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './App';
+import App from './Components/app';
 import * as serviceWorker from './serviceWorker';
-import ProjectFileReader from './Components/projectFileReader';
 
-  ReactDOM.render(<ProjectFileReader/>, document.getElementById("root"))
+  ReactDOM.render(<App/>, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
