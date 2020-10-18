d48fd6a52e6fc9b10bc26473212258d8ff2da597
Otavio Sartorelli de Toledo Piza <70035303+OtavioPiza@users.noreply.github.com>
2020-10-18 11:27:40 -0300

    Selector

--- untitled-launchpad-emulator/src/Components/App.jsx
+++ untitled-launchpad-emulator/src/Components/App.jsx
@@ -4,7 +4,18 @@ import ProjectFileReader from './ProjectFileReader'
 import Button from './Button'
 import Canvas from './Canvas'
 import deviceConfigs from '../deviceConfigs';
+import Selector from './Selector';
 
+
+const options = [
+  {
+    name: 'h',
+    value: 'h'
+  }
+]
+
+
+  
 class App extends Component {
   state = {  
     projectFile: undefined,
@@ -25,6 +36,11 @@ class App extends Component {
         {/* <Button color={"#000000"} overlay={"#808080"} x={0} y={0} on={this.on} off={this.off}/> */}
         <div className='button-container'>
           <Canvas projectFile={this.state.projectFile} deviceConfig={this.state.deviceConfig}/>
+        </div>
+        <div>
+          <Selector className='test' name='test'/>
+          <Selector className='test' name='test'/>
+          <Selector className='test' name='test'/>
         </div>
         </div>
       </React.Fragment>
--- untitled-launchpad-emulator/src/Components/Selector.jsx
+++ untitled-launchpad-emulator/src/Components/Selector.jsx
@@ -1,15 +1,11 @@
 import React, { Component } from 'react'
 
-const Selector = ({ className, name, options }) => {
+const Selector = ({ className, name, options }) => (
     <div className={className}>
         <select name={name}>
-            {options.array.forEach(element => {
-                return (
-                    <option value={element.value}>{element.name}</option>
-                )
-            })}
+            <option>Test</option>
         </select>
     </div>
-}
+)
 
 export default Selector
