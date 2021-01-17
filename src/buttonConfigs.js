3b867810212052089ade87941c5f49ab00c28f59
NuLL <cainengzhuo@gmail.com>
2021-01-17 02:15:41 -0500

    Launchpad Pro Mk3 Layout
    JK, it's a lot better than Launchpad Pro mk3

--- src/buttonConfigs.js
+++ src/buttonConfigs.js
@@ -18,21 +18,47 @@ const buttonConfigs = {
     class: "LEDButtonCircle75",
     overlayClass: "LEDButtonCircle75Overlay"
   },
-  "◪":
+  "◸":
   {
     class: "LEDButtonSquare",
   },
-  "⬕":
+  "◹":
   {
     class: "LEDButtonSquare",
   },
-  "⬔":
+  "◺":
   {
     class: "LEDButtonSquare",
   },
-  "◩":
+  "⊿":
   {
     class: "LEDButtonSquare",
+  },
+  "◤":
+  {
+    class: "LEDButtonSquare",
+    overlayClass: "LEDButtonSquareOverlay"
+  },
+  "◥":
+  {
+    class: "LEDButtonSquare",
+    overlayClass: "LEDButtonSquareOverlay"
+  },
+  "◣":
+  {
+    class: "LEDButtonSquare",
+    overlayClass: "LEDButtonSquareOverlay"
+  },
+  "◢":
+  {
+    class: "LEDButtonSquare",
+    overlayClass: "LEDButtonSquareOverlay"
+  },
+  //Launchpad Pro MK3
+  "▬":
+  {
+    class: "LEDButtonRectangle",
+    overlayClass: "LEDButtonRectangleOverlay"
   },
 }
 
